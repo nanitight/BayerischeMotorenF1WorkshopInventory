fb3af296f5401f80f34beea25ce7d4b616252312
GN Naane <gnaane@sts3d.co.za>
2026-04-21 02:43:29 +0200

    resusable result form

--- Frontend/BM-F1-Frontend/src/Components/ResultsDashboard.tsx
+++ Frontend/BM-F1-Frontend/src/Components/ResultsDashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import type { GrandPrixResult } from '../Interfaces/GrandPrixResult'
+import type { DbTrackedGrandPrixResult, GrandPrixResult } from '../Interfaces/GrandPrixResult'
 import axios from 'axios'
 import { useAppContext } from './Reusable/AppContext'
 import ResultsTable from './Render/ResultsTable';
@@ -8,37 +8,72 @@ import { Link } from 'react-router-dom';
 export default function ResultsDashboard() {
 
     const {url,loggedInUser,setLoading,loading,errorMsg,setErrorMsg} = useAppContext() ;
-    const [results, setResults] = useState<GrandPrixResult[]>( [] as GrandPrixResult[])
+    const [results, setResults] = useState<DbTrackedGrandPrixResult[]>( [] as DbTrackedGrandPrixResult[])
     
-    useEffect(()=>{
-        const fetchData = async ()=>{
-            console.log("fetching...") ;
-            setLoading(true);
-            try{
-                const resp = await axios.get(`${url}/api/GrandPrixResults`,{
-                    headers : { Authorization : `Bearer ${loggedInUser.token}`}
-                }) ;
-                if (resp.data){
-                    setResults(resp.data) ;
-                    setErrorMsg("") ;
-                }
-                console.log(resp) ;
+    const fetchData = async ()=>{
+        console.log("fetching...") ;
+        setLoading(true);
+        try{
+            const resp = await axios.get(`${url}/api/GrandPrixResults`,{
+                headers : { Authorization : `Bearer ${loggedInUser.token}`}
+            }) ;
+            if (resp.data){
+                setResults(resp.data) ;
+                setErrorMsg("") ;
             }
-            catch(err){
-                if (err.response){
-                    if (err.response.status >= 400)
-                    setErrorMsg("something went wrong... "+err.response.data) ;
-                    console.log("error occ: ", err.response);
-                }
-                else{
-                    console.log("error occ: ", err);
+            console.log(resp) ;
+        }
+        catch(err){
+            if (err.response){
+                if (err.response.status >= 400)
+                setErrorMsg("something went wrong... "+err.response.data) ;
+                console.log("error occ: ", err.response);
+            }
+            else{
+                console.log("error occ: ", err);
 
-                }
-            }
-            finally{
-                setLoading(false)
             }
         }
+        finally{
+            setLoading(false)
+        }
+    }
+
+
+    const deleteResult = async(id:string)=>{
+        var delAction = confirm("Are you sure you want to delete record? ") ; 
+        if (id == "" || id.length< 3 || !delAction) return 
+
+         setLoading(true);
+        try{
+            const resp = await axios.delete(`${url}/api/GrandPrixResults/Delete/${id}`,{
+                headers : { Authorization : `Bearer ${loggedInUser.token}`}
+            }) ;
+            console.log(resp) ;
+            if (resp.data){
+                setErrorMsg("") ;
+                await fetchData();
+            }
+        }
+        catch(err){
+            if (err.response){
+                if (err.response.status == 403)
+                    setErrorMsg("You are not Authorized for that action") ;
+                else if (err.response.status >= 400)
+                    setErrorMsg("something went wrong... "+err.response.data) ;
+                console.log("error occ: ", err.response);
+            }
+            else{
+                console.log("error occ: ", err);
+
+            }
+        }
+        finally{
+            setLoading(false)
+        }
+    }
+
+    useEffect(()=>{
         fetchData() ;
     },[])
 
@@ -54,7 +89,12 @@ export default function ResultsDashboard() {
         </h1>
 
         {results.length <= 0 ? 
-            <>There's nothing do you want to add a result?</>
+            <>There's nothing do you want to add a result? 
+                <Link to="/addresult    " className="btn btn-success bg-white text-black border-[#e5e5e5]">
+                   <svg fill="#000000" height="16px" width="16px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 330 330" xmlSpace="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M281.672,48.328C250.508,17.163,209.073,0,164.999,0C120.927,0,79.492,17.163,48.328,48.328 c-64.333,64.334-64.333,169.011,0,233.345C79.492,312.837,120.927,330,165,330c44.073,0,85.508-17.163,116.672-48.328 C346.005,217.339,346.005,112.661,281.672,48.328z M260.46,260.46C234.961,285.957,201.06,300,165,300 c-36.06,0-69.961-14.043-95.46-39.54c-52.636-52.637-52.636-138.282,0-190.919C95.039,44.042,128.94,30,164.999,30 c36.06,0,69.961,14.042,95.46,39.54C313.095,122.177,313.095,207.823,260.46,260.46z"></path> <path d="M254.999,150H180V75c0-8.284-6.716-15-15-15s-15,6.716-15,15v75H75c-8.284,0-15,6.716-15,15s6.716,15,15,15h75v75 c0,8.284,6.716,15,15,15s15-6.716,15-15v-75h74.999c8.284,0,15-6.716,15-15S263.284,150,254.999,150z"></path> </g> </g></svg>
+                    Add Result
+                </Link>
+            </>
             : 
             <div className="overflow-x-auto">
                 <Link to="/addresult    " className="btn bg-white text-black border-[#e5e5e5]">
@@ -74,9 +114,9 @@ export default function ResultsDashboard() {
                     </tr>
                     </thead>
                     <tbody>            
-                        {results.map((r : GrandPrixResult, ind : number)=> (
+                        {results.map((r : DbTrackedGrandPrixResult, ind : number)=> (
                             
-                            <ResultsTable key={ind} result={r}/>
+                            <ResultsTable key={ind} deleteFunc={deleteResult} result={r}/>
                         ))}
                     </tbody>
                 </table>
---
+++ Frontend/BM-F1-Frontend/src/Components/Reusable/GrandPrixResultForm.tsx
@@ -0,0 +1,50 @@
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { grandPrixResultSchema, type GrandPrixResultZod } from '../../Interfaces/GrandPrixResultSchema';
+
+interface ReUseForm{
+    submitFunc: (res: GrandPrixResultZod) => Promise<void>
+}
+
+export default function GrandPrixResultForm({submitFunc}:ReUseForm) {
+     const {register,handleSubmit, formState : {errors}} = useForm<GrandPrixResultZod>({
+        resolver  : zodResolver(grandPrixResultSchema), defaultValues : {
+            PointsScored : 0.0 ,
+            PositionInTeamGrid:10, 
+            RaceDay: new Date(), //.,
+            Location: ""
+
+        }}) ;
+  return (
+    <form onSubmit={handleSubmit(submitFunc)}>
+        <label className='input '>
+            <svg width="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                Location
+            <input {...register("Location")} type="text" className="input " placeholder="Location"/>
+        </label>
+            <label className='input '>
+            <svg width="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20 10V7C20 5.89543 19.1046 5 18 5H6C4.89543 5 4 5.89543 4 7V10M20 10V19C20 20.1046 19.1046 21 18 21H6C4.89543 21 4 20.1046 4 19V10M20 10H4M8 3V7M16 3V7" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path> <rect x="6" y="12" width="3" height="3" rx="0.5" fill="#000000"></rect> <rect x="10.5" y="12" width="3" height="3" rx="0.5" fill="#000000"></rect> <rect x="15" y="12" width="3" height="3" rx="0.5" fill="#000000"></rect> </g></svg>
+            Race Day
+            <input {...register("RaceDay",{valueAsDate:true})} type="date" className="input " placeholder="Raceday"/>
+        </label>
+        {errors.RaceDay && <p className='text-red-500'>{errors.RaceDay.message}</p>}
+            <label className='input '>
+            <svg  width="24px" viewBox="0 0 1024 1024" className="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M905.92 237.76a32 32 0 0 0-52.48 36.48A416 416 0 1 1 96 512a418.56 418.56 0 0 1 297.28-398.72 32 32 0 1 0-18.24-61.44A480 480 0 1 0 992 512a477.12 477.12 0 0 0-86.08-274.24z" fill="#231815"></path><path d="M630.72 113.28A413.76 413.76 0 0 1 768 185.28a32 32 0 0 0 39.68-50.24 476.8 476.8 0 0 0-160-83.2 32 32 0 0 0-18.24 61.44zM489.28 86.72a36.8 36.8 0 0 0 10.56 6.72 30.08 30.08 0 0 0 24.32 0 37.12 37.12 0 0 0 10.56-6.72A32 32 0 0 0 544 64a33.6 33.6 0 0 0-9.28-22.72A32 32 0 0 0 505.6 32a20.8 20.8 0 0 0-5.76 1.92 23.68 23.68 0 0 0-5.76 2.88l-4.8 3.84a32 32 0 0 0-6.72 10.56A32 32 0 0 0 480 64a32 32 0 0 0 2.56 12.16 37.12 37.12 0 0 0 6.72 10.56zM355.84 313.6a36.8 36.8 0 0 0-13.12 18.56l-107.52 312.96a37.44 37.44 0 0 0 2.56 35.52 32 32 0 0 0 24.96 10.56 27.84 27.84 0 0 0 17.28-5.76 43.84 43.84 0 0 0 10.56-13.44 100.16 100.16 0 0 0 7.04-15.36l4.8-12.8 17.6-49.92h118.72l24.96 69.76a45.76 45.76 0 0 0 10.88 19.2 28.8 28.8 0 0 0 20.48 8.32h2.24a27.52 27.52 0 0 0 27.84-15.68 41.28 41.28 0 0 0 0-29.44l-107.84-313.6a36.8 36.8 0 0 0-13.44-19.2 44.16 44.16 0 0 0-48 0.32z m24.32 96l41.6 125.44h-83.2zM594.88 544a66.56 66.56 0 0 0 25.6 4.16h62.4v78.72a29.12 29.12 0 0 0 32 32 26.24 26.24 0 0 0 27.2-16.32 73.28 73.28 0 0 0 4.16-26.24v-66.88h73.6a27.84 27.84 0 0 0 29.44-32 26.56 26.56 0 0 0-16-27.2 64 64 0 0 0-23.04-4.16h-64v-75.84a28.16 28.16 0 0 0-32-30.08 26.56 26.56 0 0 0-27.2 15.68 64 64 0 0 0-4.16 24v66.88h-62.72a69.44 69.44 0 0 0-25.6 4.16 26.56 26.56 0 0 0-15.68 27.2 25.92 25.92 0 0 0 16 25.92z" fill="#231815"></path></g></svg>
+            Points
+            <input {...register("PointsScored",{valueAsNumber:true})} type="number" className="input validator" placeholder="Total points scored"/>
+        </label>
+        {errors.PointsScored && <p className='text-red-500'>{errors.PointsScored.message}</p>}
+        
+            <label className='input '>
+            <svg  width="24px" viewBox="0 0 1024 1024" className="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M905.92 237.76a32 32 0 0 0-52.48 36.48A416 416 0 1 1 96 512a418.56 418.56 0 0 1 297.28-398.72 32 32 0 1 0-18.24-61.44A480 480 0 1 0 992 512a477.12 477.12 0 0 0-86.08-274.24z" fill="#231815"></path><path d="M630.72 113.28A413.76 413.76 0 0 1 768 185.28a32 32 0 0 0 39.68-50.24 476.8 476.8 0 0 0-160-83.2 32 32 0 0 0-18.24 61.44zM489.28 86.72a36.8 36.8 0 0 0 10.56 6.72 30.08 30.08 0 0 0 24.32 0 37.12 37.12 0 0 0 10.56-6.72A32 32 0 0 0 544 64a33.6 33.6 0 0 0-9.28-22.72A32 32 0 0 0 505.6 32a20.8 20.8 0 0 0-5.76 1.92 23.68 23.68 0 0 0-5.76 2.88l-4.8 3.84a32 32 0 0 0-6.72 10.56A32 32 0 0 0 480 64a32 32 0 0 0 2.56 12.16 37.12 37.12 0 0 0 6.72 10.56zM355.84 313.6a36.8 36.8 0 0 0-13.12 18.56l-107.52 312.96a37.44 37.44 0 0 0 2.56 35.52 32 32 0 0 0 24.96 10.56 27.84 27.84 0 0 0 17.28-5.76 43.84 43.84 0 0 0 10.56-13.44 100.16 100.16 0 0 0 7.04-15.36l4.8-12.8 17.6-49.92h118.72l24.96 69.76a45.76 45.76 0 0 0 10.88 19.2 28.8 28.8 0 0 0 20.48 8.32h2.24a27.52 27.52 0 0 0 27.84-15.68 41.28 41.28 0 0 0 0-29.44l-107.84-313.6a36.8 36.8 0 0 0-13.44-19.2 44.16 44.16 0 0 0-48 0.32z m24.32 96l41.6 125.44h-83.2zM594.88 544a66.56 66.56 0 0 0 25.6 4.16h62.4v78.72a29.12 29.12 0 0 0 32 32 26.24 26.24 0 0 0 27.2-16.32 73.28 73.28 0 0 0 4.16-26.24v-66.88h73.6a27.84 27.84 0 0 0 29.44-32 26.56 26.56 0 0 0-16-27.2 64 64 0 0 0-23.04-4.16h-64v-75.84a28.16 28.16 0 0 0-32-30.08 26.56 26.56 0 0 0-27.2 15.68 64 64 0 0 0-4.16 24v66.88h-62.72a69.44 69.44 0 0 0-25.6 4.16 26.56 26.56 0 0 0-15.68 27.2 25.92 25.92 0 0 0 16 25.92z" fill="#231815"></path></g></svg>
+            Points
+            <input {...register("PositionInTeamGrid",{valueAsNumber:true})} type="number" className="input validator" placeholder="Team Grid Position"/>
+        </label>
+        {errors.PositionInTeamGrid && <p className='text-red-500'>{errors.PositionInTeamGrid.message}</p>}
+        
+        <div className="card-actions justify-end">
+        <button type='submit' className="btn btn-primary">Add</button>
+        </div>
+    </form>
+  )
+}
