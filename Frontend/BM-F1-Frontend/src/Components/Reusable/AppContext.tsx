deded9b0a1a445423b11964a74deadf6d769f37c
GN Naane <gnaane@sts3d.co.za>
2026-04-21 09:35:02 +0200

    frontend deployed

--- Frontend/BM-F1-Frontend/src/Components/Reusable/AppContext.tsx
+++ Frontend/BM-F1-Frontend/src/Components/Reusable/AppContext.tsx
@@ -6,7 +6,9 @@ import type { DbTrackedGrandPrixResult } from "../../Interfaces/GrandPrixResult"
 const AppContext = createContext<AppContext | undefined>(undefined) ;
 
 export const AppContextProvider : React.FC<{ children: React.ReactNode }> = ({children} ) =>{
-    const url = "http://localhost:5014" ;
+    // const url = "http://localhost:5014" ;
+    const url = "https://bmf-9cvpu62s.b4a.run" ;
+    
     const [loggedInUser,setLoggedInUser] = useState<LoggedInUser>({} as LoggedInUser) ;
     const [loading, setLoading] = useState(false) ;
     const [errorMsg, setErrorMsg] = useState<string>("")
